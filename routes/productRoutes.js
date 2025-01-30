3c5ce4b88a7f31ce5cef09e0b9b1dad954815495
Your Name <khouloudmk96@gmail.com>
2025-01-30 11:47:08 +0100

    updated backend : added review and pdf

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,7 +4,11 @@ const productController = require('../controllers/productController');
 
 router.post('/createProduct', productController.createProduct);
 router.get('/getProducts', productController.getProducts);
+router.get('/getProduct/:id', productController.getProduct);
 router.put('/updateProduct/:id', productController.updateProduct);
 router.delete('/deleteProduct/:id', productController.deleteProduct);
+router.post('/createReview/:id/reviews', productController.createReview);
+router.get('/getReviews/:id/reviews', productController.getReviews);
+router.get('/pdf', productController.generatePdf);
 
 module.exports = router;
